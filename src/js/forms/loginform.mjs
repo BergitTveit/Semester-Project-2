cbd983d7fdee4844625808b8e33db54ccbba8736
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-09-10 14:20:12 +0200

    Setup prettier as a precommit git hook

--- src/js/forms/loginform.mjs
+++ src/js/forms/loginform.mjs
@@ -1,31 +1,24 @@
-import { createButton, createInputField } from "../components.mjs";
-import {
-  emailValidation,
-  passwordValidation,
-} from "../validations/validators.mjs";
+import { createButton, createInputField } from '../components.mjs';
+import { emailValidation, passwordValidation } from '../validations/validators.mjs';
 
 export function initializeLoginForm() {
-  const form = document.getElementById("loginForm");
+    const form = document.getElementById('loginForm');
 
-  form.appendChild(
-    createInputField({
-      type: "email",
-      placeholder: "Email",
-      validationFn: emailValidation,
-    })
-  );
+    form.appendChild(
+        createInputField({
+            type: 'email',
+            placeholder: 'Email',
+            validationFn: emailValidation,
+        })
+    );
 
-  form.appendChild(
-    createInputField({
-      type: "password",
-      placeholder: "Password",
-      validationFn: passwordValidation,
-    })
-  );
-  form.appendChild(
-    createButton("Register", () => console.log("Register button clicked"))
-  );
-  form.appendChild(
-    createButton("Login", () => console.log("Login button clicked"))
-  );
+    form.appendChild(
+        createInputField({
+            type: 'password',
+            placeholder: 'Password',
+            validationFn: passwordValidation,
+        })
+    );
+    form.appendChild(createButton('Register', () => console.log('Register button clicked')));
+    form.appendChild(createButton('Login', () => console.log('Login button clicked')));
 }
